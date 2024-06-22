185d94fbe5e38709ff117259cf11c3bea2211d74
Jamis Hartley <jamman98@gmail.com>
2024-06-22 03:32:51 -0600

    Fix some deprecation errors and warnings

--- hosts/breadbox/configuration.nix
+++ hosts/breadbox/configuration.nix
@@ -171,10 +171,9 @@
   # networking.firewall.enable = false;
 
   # Enable OpenGL
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+    enable32Bit = true;
   };
 
 
