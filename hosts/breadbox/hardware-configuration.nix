9dc14b11d9bf3f22287949ccfff2079925444a39
Jamis Hartley <jamman98@gmail.com>
2025-01-31 18:17:36 -0700

    Mount appdata

--- hosts/breadbox/hardware-configuration.nix
+++ hosts/breadbox/hardware-configuration.nix
@@ -57,6 +57,18 @@
       [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100" ];
   };
 
+  fileSystems."/mnt/kent-appdata" = {
+    device = "//10.0.1.100/appdata";
+    fsType = "cifs";
+    options =
+      let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in
+      [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100" ];
+  };
+
   fileSystems."/mnt/windrive" = {
     device = "/dev/disk/by-uuid/5600B9A700B98E8D";
     fsType = "ntfs-3g";
