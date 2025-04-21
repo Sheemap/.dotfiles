c8d6c9953b83cf57410ace5aceecc1623023d999
Jamis Hartley <jamman98@gmail.com>
2025-04-21 16:17:54 -0600

    Remove incorrect cifs param

--- hosts/caroline/hardware-configuration.nix
+++ hosts/caroline/hardware-configuration.nix
@@ -42,7 +42,7 @@
     options =
       let
         # this line prevents hanging on network split
-        automount_opts = "x-systemd.automount,auto,x-systemd.device-timeout=15s,x-systemd.mount-timeout=15s,retry=10000";
+        automount_opts = "x-systemd.automount,auto,x-systemd.device-timeout=15s,x-systemd.mount-timeout=infinity";
 
       in
       [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100" ];
